a0df4ac98120b7413625181149a8adac839e09fc
justplay0526 <justplay0526@gmail.com>
2024-01-12 20:44:29 +0800

    finish java mora fun

--- java/lab3/app/src/main/java/com/practice/mmora/MainActivity.java
+++ java/lab3/app/src/main/java/com/practice/mmora/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             }
             String playerName = binding.edName.getText().toString();
 
-            int comMora = (int)(Math.random() *3);
+            int comMora = (int)(Math.random() * 3);
 
             String playerMoraText = "";
             if(binding.btnScissor.isChecked()){
@@ -35,14 +35,29 @@ public class MainActivity extends AppCompatActivity {
 
             String comMoraText = "";
             switch(comMora){
-                case  0: comMoraText = "剪刀";
-                case  1: comMoraText = "石頭";
-                default: comMoraText = "布";
+                case  0: comMoraText = "剪刀";break;
+                case  1: comMoraText = "石頭";break;
+                default: comMoraText = "布";break;
             }
 
-            binding.tvText.setText(String.format("名字\n%s", binding.edName.getText().toString()));
+            binding.tvName.setText(String.format("名字\n%s", binding.edName.getText().toString()));
             binding.tvMmora.setText(String.format("我方出拳\n%s", playerMoraText));
             binding.tvCmora.setText(String.format("電腦出拳\n%s", comMoraText));
+
+            if((binding.btnScissor.isChecked() && comMora == 2) ||
+                    (binding.btnStone.isChecked() && comMora == 0) ||
+                    (binding.btnPaper.isChecked() && comMora == 1)){
+                binding.tvWinner.setText(String.format("勝利者\n%s", binding.edName.getText().toString()));
+                binding.tvText.setText("恭喜你獲勝了");
+            } else if ((binding.btnScissor.isChecked() && comMora == 1) ||
+                    (binding.btnStone.isChecked() && comMora == 2) ||
+                    (binding.btnPaper.isChecked() && comMora == 0)) {
+                binding.tvWinner.setText("勝利者\n電腦");
+                binding.tvText.setText("可惜，電腦獲勝了!");
+            }else {
+                binding.tvWinner.setText("勝利者\n平手");
+                binding.tvText.setText("平局，請再試一次");
+            }
         }
 
         );
