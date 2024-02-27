0df337f65ddf211be307c5e8366888a31f942a2a
justplay0526 <justplay0526@gmail.com>
2024-02-27 21:34:00 +0800

    finish kt lab7

--- kotlin/lab7/app/src/main/java/com/practice/listview/MainActivity.kt
+++ kotlin/lab7/app/src/main/java/com/practice/listview/MainActivity.kt
@@ -2,11 +2,36 @@ package com.practice.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.practice.listview.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val count = ArrayList<String>()
+        val item = ArrayList<Item>()
+        val priceRange = IntRange(10, 100)
+        val img_lst =
+            resources.obtainTypedArray(R.array.image_list)
+        for (i in 0 until img_lst.length()){
+            val photo = img_lst.getResourceId(i, 0)
+            val name = "水果${i+1}"
+            val price = priceRange.random()
+            count.add("${i+1}個")
+            item.add(Item(photo, name, price))
+        }
+        img_lst.recycle()
+
+        binding.spSpinner.adapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, count)
+
+        binding.gridView.numColumns = 3
+        binding.gridView.adapter = MyAdapter(this, item, R.layout.adapter_vert)
+        binding.listView.adapter = MyAdapter(this, item, R.layout.adapter_horizon)
     }
 }
 
