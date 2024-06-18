e62dc462a3addef6bf43cb992cbe2c0b43f6125a
justplay0526 <justplay0526@gmail.com>
2024-06-18 15:49:24 +0800

    add MainAct comment

--- kotlin/AndroidQuiz/app/src/main/java/com/practice/androidquiz/MainActivity.kt
+++ kotlin/AndroidQuiz/app/src/main/java/com/practice/androidquiz/MainActivity.kt
@@ -111,7 +111,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
         binding.edSearch.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_UP) {
-                val clearDrawable: Drawable? = binding.edSearch.compoundDrawables[2] // 獲得右邊的 drawable
+                val clearDrawable: Drawable? = binding.edSearch.compoundDrawables[2] //獲得右邊的drawable
                 if (clearDrawable != null && event.rawX >= (binding.edSearch.right
                             - binding.edSearch.paddingEnd - clearDrawable.bounds.width())) {
                     binding.edSearch.text.clear()
@@ -120,11 +120,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             }
             false
         }
-
         //載入地圖
         loadMap()
-
-        // 初始化 FusedLocationProviderClient
+        //初始化FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         //取得資料庫實體
         dbrw = MyDBHelper(this).writableDatabase
@@ -155,7 +153,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     }
     //向API丟出請求
     private fun sendRequest(){
-        val url = "https://android-quiz-29a4c.web.app/"
+        val url = "https://android-quiz-29a4c.web.app/" //API的URL
         val req = okhttp3.Request.Builder()
             .url(url)
             .build()
@@ -178,18 +176,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                                 arrayOf(data.name, data.vicinity, data.lat.toFloat(), data.lng.toFloat(),
                                     0, data.star, data.photo, landscapeUrl))
                         }
-                        //關閉指標
-                        cursor.close()
-                        //將 marker 標註在地圖上
+                        cursor.close() //關閉指標
+                        //將marker標註在地圖上
                         marker.position(LatLng(data.lat.toDouble(), data.lng.toDouble()))
                         marker.title(data.name)
-                        marker.draggable(true)
-                        maps.addMarker(marker)
+                        marker.draggable(true) //讓marker可以點擊
+                        maps.addMarker(marker) //在地圖上加入marker
                     }
                 }
-
             }
-
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread{
                     Toast.makeText(this@MainActivity, "查詢失敗$e", Toast.LENGTH_SHORT).show()
@@ -203,33 +198,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         binding.btnSearch.visibility = View.VISIBLE
         binding.btnHistory.visibility = View.VISIBLE
     }
-
+    //顯示搜尋的Dialog
     private fun showSearchDialog(){
-        val results = searchByName(binding.edSearch.text.toString())
-        if (results.toString() == "[]"){
+        val results = searchByName(binding.edSearch.text.toString()) //從資料庫裡面抓出的欄位放入item
+        if (results.toString() == "[]"){ //若是fun回傳[]，代表資料庫裡找不到資料
             Toast.makeText(this@MainActivity, "查無紀錄", Toast.LENGTH_SHORT).show()
         } else {
             val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_search, null)
             val builder = AlertDialog.Builder(this)
             builder.setView(dialogView)
-            builder.setCancelable(true)
+            builder.setCancelable(true) //讓Dialog能夠透過點擊View範圍外進行Dialog的銷毀
             // 建立 AlertDialog
             val dialog = builder.create()
+            //設定Dialog的自定義背景，不能以xml設定，否則無效
             dialog.window?.setBackgroundDrawableResource(R.drawable.round_dialog_bg)
-            dialog.show()
-//            results.forEach{c ->
-//                val name = c["name"] as String
-//            }
+            dialog.show() //顯示Dialog
             //宣告Adapter
             searchAdapter = ListAdapter(this, results)
             val lsvSql = dialogView.findViewById<ListView>(R.id.lsv_sql)
             lsvSql.adapter = searchAdapter
-            lsvSql.onItemClickListener = AdapterView.OnItemClickListener{ _, view, _, _ ->
-                val text = view.findViewById<TextView>(R.id.tvName).text.toString()
+            //設定ListView的Listener
+            lsvSql.onItemClickListener = AdapterView.OnItemClickListener{ _, view, _, _ -> //其他參數不必要所以用_表示
+                val text = view.findViewById<TextView>(R.id.tvName).text.toString() //取出item內的旅館名稱
                 dbrw.execSQL("UPDATE apiTable SET READ = 1 WHERE name LIKE '${text}'")
                 val cursor = dbrw.rawQuery("SELECT * FROM apiTable WHERE name LIKE ?", arrayOf(text))
                 cursor.moveToFirst()
-                val locate = text to LatLng(
+                val locate = text to LatLng( //讓名稱跟經緯度形成對應
                         cursor.getDouble(cursor.getColumnIndexOrThrow("lat")),
                         cursor.getDouble(cursor.getColumnIndexOrThrow("lng"))
                     )
@@ -238,12 +232,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 maps.moveCamera(CameraUpdateFactory.newLatLngZoom(
                     locate.second, 13f
                 ))
-                //移動完將dialog隱藏
+                //移動完將dialog銷毀
                 dialog.dismiss()
             }
         }
     }
-
+    //顯示搜尋紀錄的Dialog
     private fun showHistoryDialog(){
         val cursor = dbrw.rawQuery("SELECT * FROM apiTable WHERE read = 1", null)
         if (cursor.count == 0){
@@ -255,14 +249,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             builder.setCancelable(true)
             // 建立 AlertDialog
             val dialog = builder.create()
+            //設定Dialog的自定義背景，不能以xml設定，否則無效
             dialog.window?.setBackgroundDrawableResource(R.drawable.round_dialog_bg)
-            dialog.show()
+            dialog.show() //顯示Dialog
 
             val results = mutableListOf<Map<String, Any>>()
             historyAdapter = ListAdapter(this, results)
             val lsvHistory = dialogView.findViewById<ListView>(R.id.lsv_history)
             lsvHistory.adapter = historyAdapter
 
+            while (cursor.moveToNext()) {
+                val result = mutableMapOf<String, Any>() //從資料庫裡面抓出的欄位放入item
+                result["name"] = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                result["vic"] = cursor.getString(cursor.getColumnIndexOrThrow("vic"))
+                results.add(result)
+            }
+            cursor.close()
+            //更新historyAdapter
+            historyAdapter.notifyDataSetChanged()
+            //設定ListView的Listener
             lsvHistory.onItemClickListener = AdapterView.OnItemClickListener{ _, view, _, _ ->
                 val text = view.findViewById<TextView>(R.id.tvName).text.toString()
                 dbrw.execSQL("UPDATE apiTable SET READ = 1 WHERE name LIKE '${text}'")
@@ -280,24 +285,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 //移動完將dialog隱藏
                 dialog.dismiss()
             }
-
-            while (cursor.moveToNext()) {
-                val result = mutableMapOf<String, Any>()
-                result["name"] = cursor.getString(cursor.getColumnIndexOrThrow("name"))
-                result["vic"] = cursor.getString(cursor.getColumnIndexOrThrow("vic"))
-                results.add(result)
-            }
-            cursor.close()
-
-            historyAdapter.notifyDataSetChanged()
-
+            //清除搜尋紀錄
             dialogView.findViewById<Button>(R.id.btn_clear).setOnClickListener {
-                dbrw.execSQL("UPDATE apiTable SET READ = 0")
+                dbrw.execSQL("UPDATE apiTable SET READ = 0")//將已讀紀錄更新為未讀
                 Toast.makeText(this@MainActivity, "已清除紀錄", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
             }
         }
     }
+    //顯示景點資料的Dialog
     private fun showMarkerDialog(marker: Marker) {
         val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_info, null)
         val builder = AlertDialog.Builder(this)
@@ -308,27 +304,28 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         dialog.window?.setBackgroundDrawableResource(R.drawable.round_info_bg)
         val tvTitle = dialogView.findViewById<TextView>(R.id.tv_title)
         tvTitle.text = marker.title
-        dialog.show()
+        dialog.show()//顯示Dialog
+        //宣告Dialog中的兩個按鈕
         val btnHotel = dialogView.findViewById<Button>(R.id.btn_hotel)
         val btnGuide = dialogView.findViewById<Button>(R.id.btn_guide)
         btnHotel.setOnClickListener {
             val cursor = dbrw.rawQuery("SELECT * FROM apiTable WHERE name LIKE ?", arrayOf(marker.title))
             cursor.moveToFirst()
 
-            val frag = DetailFragment()
-            val b = Bundle()
+            val frag = DetailFragment() //建立詳細資訊頁面的Fragment
+            val b = Bundle() //建立一個Bundle以方便傳遞資料
             b.putString("name", marker.title)
             b.putString("vic", cursor.getString(cursor.getColumnIndexOrThrow("vic")))
             b.putString("star", cursor.getString(cursor.getColumnIndexOrThrow("star")))
             b.putString("photo", cursor.getString(cursor.getColumnIndexOrThrow("photo")))
             b.putString("landscape", cursor.getString(cursor.getColumnIndexOrThrow("landscape")))
-            frag.arguments = b
-            supportFragmentManager.beginTransaction()
+            frag.arguments = b //透過arguments將Bundle傳遞到DetailFragment
+            supportFragmentManager.beginTransaction() //開啟Fragment
                 .replace(R.id.container, frag)
                 .addToBackStack(null)
                 .commit()
             cursor.close()
-
+            //將按鈕的可見度設為不可見
             binding.btnSearch.visibility = View.INVISIBLE
             binding.btnHistory.visibility = View.INVISIBLE
 
@@ -337,13 +334,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         btnGuide.setOnClickListener {
             val origin = currentLocation
             val dest = marker.title
-            openGoogleMapsDirections(origin, dest.toString())
+            openGoogleMapsDirections(origin, dest.toString()) //開啟Google地圖的導航
         }
 
     }
 
     private fun openGoogleMapsDirections(origin: String, destination: String) {
-        val uri = Uri.parse("https://www.google.com/maps/dir/?api=1&origin=$origin&destination=$destination&travelmode=driving")
+        val uri = Uri.parse("https://www.google.com/maps/dir/?api=1&origin=" +
+                "$origin&destination=$destination&travelmode=driving")
         val intent = Intent(Intent.ACTION_VIEW, uri)
         intent.setPackage("com.google.android.apps.maps")
 
@@ -354,8 +352,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             startActivity(browserIntent)
         }
     }
-
-
+    //透過關鍵字在資料庫裡搜尋包含關鍵字的景點
     private fun searchByName(keyword: String): MutableList<Map<String, Any>> {
         val dbHelper = MyDBHelper(this)
         val db = dbHelper.readableDatabase
@@ -378,8 +375,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
         return results
     }
-
-    class MyObject {
+    class MyObject { //用來接Gson轉出的資料格式
         lateinit var results: Result
         class Result {
             lateinit var content: Array<Content>
@@ -394,7 +390,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             }
         }
     }
-    class MyDBHelper(
+    class MyDBHelper(//建立本地的資料庫用的DBHelper
         context: Context,
         name: String = database,
         factory: CursorFactory ?= null,
